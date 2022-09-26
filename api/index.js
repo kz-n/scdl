ebaebcac6e0825a114bc9cfb97d2d926a79fcfff
kz-n <64893077+kz-n@users.noreply.github.com>
2022-09-26 15:54:17 +0200

    testing

---
+++ api/index.js
@@ -0,0 +1,16 @@
+import {VercelRequest, VercelResponse} from "@vercel/node";
+
+const scdl = require('soundcloud-downloader').default
+const fs = require('fs')
+
+const SOUNDCLOUD_URL = 'https://soundcloud.com/1dashel/lya'
+const CLIENT_ID = 'asdhkalshdkhsf'
+
+let count = 1
+
+export default function handler(request, response) {
+    count++
+    response.write(String(count))
+    response.end()
+    //scdl.download(SOUNDCLOUD_URL).then(stream => stream.pipe(response))
+}
